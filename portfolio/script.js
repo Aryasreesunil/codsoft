0b9595e0b111075de943b2da53cdb57d3218d712
Aryasree <138470999+Aryasreesunil@users.noreply.github.com>
2025-07-23 11:25:25 +0530

    Add files via upload
    https://aryasree-portfolio.netlify.app/ : click here to see the execution of this project

---
+++ portfolio/script.js
@@ -0,0 +1,24 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("slide");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
